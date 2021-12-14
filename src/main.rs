6c72612a8b4b71da1b7a1907491e0a8e19f12554
Connor Mooneyhan <connmoo@gmail.com>
2021-12-13 21:53:40 -0500

    Add render method to Map

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 mod map;
 
 mod prelude {
-    use bracket_lib::prelude::*;
+    pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    use crate::map::*;    
+    pub use crate::map::*;    
 }
 
 use prelude::*;
--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,5 @@
+use bracket_lib::prelude::{BTerm, to_cp437};
+
 use crate::prelude::*;
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
@@ -7,14 +9,51 @@ pub enum TileType {
     Floor
 }
 
+/// Calculates index in Map.tiles for a given x and y\
+/// Indexed Y-first
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
 pub struct Map {
     pub tiles: Vec<TileType>
 }
 
 impl Map {
-    fn new() -> Self {
+    /// Constructs new Map instance populated with floor tiles.
+    pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES]
+        }
+    }
+
+    /// Loops through Map.tiles to render '.' for floor tiles and '#' for wall tiles.
+    pub fn render(&self, ctx: &mut BTerm) {
+        // Looping y-first is most performant with row-first striding
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(
+                            x,
+                            y,
+                            YELLOW,
+                            BLACK,
+                            to_cp437('.')
+                        );
+                    },
+                    TileType::Wall => {
+                        ctx.set(
+                            x,
+                            y,
+                            GREEN,
+                            BLACK,
+                            to_cp437('#')
+                        );
+                    }
+                }
+            }
         }
     }
 }
