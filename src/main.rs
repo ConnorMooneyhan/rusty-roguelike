34934e6a8ae62f80afcd081e29067c0554f159c0
Connor Mooneyhan <connmoo@gmail.com>
2021-12-13 22:02:53 -0500

    Build context and initialize main_loop

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,11 @@ impl GameState for State {
     }
 }
 
-fn main() {}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusty Roguelike")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
