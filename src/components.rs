f0ef35dc8d1708eb7f1dd7aa430b01be44a5b9ad
Connor Mooneyhan <connmoo@gmail.com>
2021-12-15 22:30:51 -0500

    Add empty schedule builder and implement in tick fn

--- src/components.rs
+++ src/components.rs
@@ -2,8 +2,8 @@ use crate::prelude::*;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Render {
-    color: ColorPair, // Foreground and background color
-    glyph: FontCharType, // Single character or glyph
+    pub color: ColorPair, // Foreground and background color
+    pub glyph: FontCharType, // Single character or glyph
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
---
+++ src/systems/mod.rs
@@ -0,0 +1,7 @@
+use crate::prelude::*;
+
+/// Builds Legion schedule from systems
+pub fn build_scheduler() -> Schedule {
+    Schedule::builder()
+        .build()
+}
