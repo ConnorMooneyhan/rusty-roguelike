4da9a0c97c7a9ef9f007abe26cd89f2c2282c4b8
Connor Mooneyhan <connmoo@gmail.com>
2021-12-16 22:09:13 -0500

    Define map_render system and add it to scheduler

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,10 +1,12 @@
 use crate::prelude::*;
 
 mod player_input;
+mod map_render;
 
 /// Builds Legion schedule from systems
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
         .build()
 }
