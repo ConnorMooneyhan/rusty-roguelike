7e8b545b2de084c8f6723ee5cd2546f96cae6980
Connor Mooneyhan <connmoo@gmail.com>
2021-12-19 16:25:33 -0500

    Implement movement intent for monsters

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -1,22 +1,24 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    <&mut Point>::query()
-        .filter(component::<MovingRandomly>())
-        .iter_mut(ecs)
-        .for_each(|pos| {
-            let mut rng = RandomNumberGenerator::new();
-            let destination = match rng.range(0, 4) {
-                0 => Point::new(-1, 0), // Left
-                1 => Point::new(1, 0),  // Right
-                2 => Point::new(0, -1), // Up
-                _ => Point::new(0, 1),  // Down
-            } + *pos;
-            if map.can_enter_tile(destination) {
-                *pos = destination;
-            }
-        });
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point)>::query().filter(component::<MovingRandomly>());
+    movers.iter(ecs).for_each(|(entity, pos)| {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range(0, 4) {
+            0 => Point::new(-1, 0), // Left
+            1 => Point::new(1, 0),  // Right
+            2 => Point::new(0, -1), // Up
+            _ => Point::new(0, 1),  // Down
+        } + *pos;
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
+    });
 }
