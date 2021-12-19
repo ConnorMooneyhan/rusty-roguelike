dec1f4fe02f68040db84acb870e9f181831c8a07
Connor Mooneyhan <connmoo@gmail.com>
2021-12-19 16:01:31 -0500

    Add movement system to handle messages of intent to move

---
+++ src/systems/movement.rs
@@ -0,0 +1,29 @@
+use crate::prelude::*;
+
+#[system(for_each)] // Runs system once for each entity matching arguments
+#[read_component(Player)]
+pub fn movement(
+    entity: &Entity,
+    want_move: &WantsToMove,
+    #[resource] map: &Map,
+    #[resource] camera: &mut Camera,
+    ecs: &mut SubWorld,
+    commands: &mut CommandBuffer,
+) {
+    // Checks if desired movement destination is valid
+    if map.can_enter_tile(want_move.destination) {
+        // Schedules movement to desired destination
+        commands.add_component(want_move.entity, want_move.destination);
+
+        // Center camera if entity that moved is Player
+        if ecs.entry_ref(want_move.entity)
+            .unwrap()
+            .get_component::<Player>().is_ok()
+        {
+            camera.on_player_move(want_move.destination);
+        }
+    }
+
+    // Removes message so it isn't evaluated again
+    commands.remove(*entity);
+}
