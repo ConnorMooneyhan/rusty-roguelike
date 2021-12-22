bdb8d7cd2f492eaa83ef8cd92b6380f2eac7406c
Connor Mooneyhan <connmoo@gmail.com>
2021-12-21 22:45:33 -0500

    Add health regeneration on wait

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -5,6 +5,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -27,12 +29,14 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-        if delta.x != 0 || delta.y != 0 {
+        let mut did_something = false;
+        if delta != Point::zero() {
             let mut hit_something = false;
             enemies
                 .iter(ecs)
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
+                    did_something = true;
                     hit_something = true;
                     commands.push((
                         (),
@@ -44,6 +48,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -51,6 +56,16 @@ pub fn player_input(
                         destination: destination,
                     },
                 ));
+            }
+        }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
 
