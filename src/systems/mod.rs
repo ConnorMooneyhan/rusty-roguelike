d9e2aed632485a2156e244f1fdfa1877be01184c
Connor Mooneyhan <connmoo@gmail.com>
2021-12-21 20:52:21 -0500

    Remove collisions.rs in preparation for combat

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,5 @@
 use crate::prelude::*;
 
-mod collisions;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -27,8 +26,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
@@ -43,8 +40,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
