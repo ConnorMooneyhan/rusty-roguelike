a83f5c3e2a6bc559210c6b3f39560dc7f746fea7
Connor Mooneyhan <connmoo@gmail.com>
2021-12-28 08:09:52 -0500

    Update fov on movement

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 /// Processes WantsToMove requests
 #[system(for_each)] // Runs system once for each entity matching arguments
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     want_move: &WantsToMove,
@@ -11,6 +12,16 @@ pub fn movement(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
 ) {
+    if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+        if let Ok(fov) = entry.get_component::<FieldOfView>() {
+            commands.add_component(want_move.entity, fov.clone_dirty());
+        }
+
+        if entry.get_component::<Player>().is_ok() {
+            camera.on_player_move(want_move.destination);
+        }
+    }
+    
     // Checks if desired movement destination is valid
     if map.can_enter_tile(want_move.destination) {
         // Schedules movement to desired destination
