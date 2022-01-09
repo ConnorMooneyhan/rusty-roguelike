f2613c45969e2bbd65cf851a7d373a11b8421be0
Connor Mooneyhan <connmoo@gmail.com>
2022-01-08 22:27:31 -0500

    Add NextLevel state to TurnState

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,8 @@ impl State {
         self.resources.insert(TurnState::AwaitingInput);
         self.resources.insert(map_builder.theme);
     }
+
+    fn advance_level(&mut self) {}
 }
 
 impl GameState for State {
@@ -167,6 +169,7 @@ impl GameState for State {
                 .execute(&mut self.ecs, &mut self.resources),
             TurnState::GameOver => self.game_over(ctx),
             TurnState::Victory => self.victory(ctx),
+            TurnState::NextLevel => self.advance_level(),
         }
         render_draw_buffer(ctx).expect("Render error");
     }
